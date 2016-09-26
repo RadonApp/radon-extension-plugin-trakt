55ea2e6cc1c7982462ae6f0f23ef5757d5389cd2
Dean Gardiner <me@dgardiner.net>
2016-09-26 16:04:34 +1300

    Added more configuration option definitions (not implemented yet)

--- src/services/configuration/options.js
+++ src/services/configuration/options.js
@@ -15,5 +15,66 @@ export default [
 
     new TraktAuthenticationOption(Plugin, 'authorization', 'Authentication', {
         requires: ['enabled']
-    })
+    }),
+
+    new Group(Plugin, 'scrobble', 'Scrobble', [
+        new EnableOption(Plugin, 'scrobble.enabled', 'Enabled', {
+            default: true,
+            requires: ['enabled']
+        }),
+
+        new CheckboxOption(Plugin, 'scrobble.movies', 'Movies', {
+            default: true,
+            requires: ['scrobble.enabled']
+        }),
+
+        new CheckboxOption(Plugin, 'scrobble.episodes', 'Episodes', {
+            default: true,
+            requires: ['scrobble.enabled']
+        }),
+
+        new Group(Plugin, 'scrobble.notifications', 'Notifications', [
+            new EnableOption(Plugin, 'scrobble.notifications.enabled', 'Enabled', {
+                default: true,
+                requires: ['scrobble.enabled']
+            }),
+
+            new CheckboxOption(Plugin, 'scrobble.notifications.started', 'Started', {
+                default: false,
+                requires: ['scrobble.enabled']
+            }),
+
+            new CheckboxOption(Plugin, 'scrobble.notifications.paused', 'Paused', {
+                default: false,
+                requires: ['scrobble.enabled']
+            }),
+
+            new CheckboxOption(Plugin, 'scrobble.notifications.scrobbled', 'Scrobbled', {
+                default: true,
+                requires: ['scrobble.enabled']
+            }),
+
+            new CheckboxOption(Plugin, 'scrobble.notifications.error', 'Error', {
+                default: true,
+                requires: ['scrobble.enabled']
+            }),
+        ])
+    ]),
+
+    new Group(Plugin, 'sync', 'Sync', [
+        new EnableOption(Plugin, 'sync.enabled', 'Enabled', {
+            default: true,
+            requires: ['enabled']
+        }),
+
+        new CheckboxOption(Plugin, 'sync.history', 'Watched history', {
+            default: true,
+            requires: ['sync.enabled']
+        }),
+
+        new CheckboxOption(Plugin, 'sync.ratings', 'Ratings', {
+            default: true,
+            requires: ['sync.enabled']
+        })
+    ]),
 ];
