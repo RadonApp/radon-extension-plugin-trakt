e95bf77ddd6944067562c82a447a6f5207de7e3a
Dean Gardiner <me@dgardiner.net>
2016-10-12 14:09:02 +1300

    Updated the callback module to support Firefox callback redirections

--- src/modules/callback/index.js
+++ src/modules/callback/index.js
@@ -1,28 +1,43 @@
+import {isDefined} from 'eon.extension.framework/core/helpers';
 import MessagingBus from 'eon.extension.framework/messaging/bus';
 
 import querystring from 'querystring';
 
 
 function process() {
-    // Connect to relay messaging bus
-    let bus = new MessagingBus(window.name + '/callback').connect(
-        'eon.extension.core:relay'
-    );
-
     // Validate search parameters
     if(window.location.search.length < 2) {
-        bus.relay(window.name, 'popup.reject', 'Missing search parameters');
+        console.error('Missing search parameters');
         return;
     }
 
     // Decode query string
-    let query = querystring.decode(window.location.search.substring(1));
+    let query = querystring.decode(
+        window.location.search.substring(1)
+    );
+
+    // Retrieve callback identifier
+    let id;
+
+    if(isDefined(query.id)) {
+        id = 'eon.popup/' + query.id;
+    } else if(window.name !== '') {
+        id = window.name;
+    } else {
+        console.error('Unable to retrieve callback identifier');
+        return;
+    }
+
+    // Connect to relay messaging bus
+    let bus = new MessagingBus(id + '/callback').connect(
+        'eon.extension.core:relay'
+    );
 
     // Emit response event
     if(typeof query.code !== 'undefined') {
-        bus.relay(window.name, 'popup.resolve', query.code);
+        bus.relay(id, 'popup.resolve', query.code);
     } else {
-        bus.relay(window.name, 'popup.reject', 'Unable to retrieve code');
+        bus.relay(id, 'popup.reject', 'Unable to retrieve code');
     }
 
     // Close window
